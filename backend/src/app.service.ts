f579ded23b3fb915b738b25696177abaf1619a58
Michal <kolodziejczykmichal269@gmail.com>
2022-03-28 23:20:54 +0200

    initial prisma

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from './prisma/prisma.service';
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async getHello(): Promise<object> {
+    const user = await this.prisma.user.create({
+      data: {
+        email: 'test123@gmail.com',
+        hash: 'hashe',
+        hashedRt: 'hashhes',
+      },
+    });
+
+    // console.log('USER', user);
+    // const getUser = await this.prisma.user.findMany({});
+
+    // console.log('USERSSS', getUser);
+    // return getUser;
+
+    return { msg: 'hello' };
   }
 }
