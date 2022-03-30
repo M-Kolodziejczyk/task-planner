53204f10727c7eb90e200091b10508b4eac86daf
Michal <kolodziejczykmichal269@gmail.com>
2022-03-30 20:49:58 +0200

    init setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from './prisma/prisma.service';
 
+import { Prisma } from '@prisma/client';
+
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService) {}
 
   async getHello(): Promise<object> {
+    console.log('HELLLO enpoint');
     const user = await this.prisma.user.create({
       data: {
         email: 'test123@gmail.com',
@@ -15,12 +18,16 @@ export class AppService {
       },
     });
 
+    // const users = await this.prisma.user.findMany();
+    // console.log('first', users);
+    // this.prisma.user.create
+
     // console.log('USER', user);
     // const getUser = await this.prisma.user.findMany({});
 
     // console.log('USERSSS', getUser);
     // return getUser;
 
-    return { msg: 'hello' };
+    return { msg: 'hello', user };
   }
 }
