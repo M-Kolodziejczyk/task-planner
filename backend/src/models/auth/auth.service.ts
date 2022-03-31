000e54f7ea9db8dacde19f0b2397d1a7cc71e78a
Michal <kolodziejczykmichal269@gmail.com>
2022-03-31 23:04:07 +0200

    tokkens

--- backend/src/models/auth/auth.service.ts
+++ backend/src/models/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
@@ -20,10 +20,65 @@ export class AuthService {
       },
     });
 
-    // const tokens = await this.g
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  async refreshToken(userId: number, rt: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user || !user.hashedRt) throw new ForbiddenException('Acces Denied');
+
+    const rtMatch = await bcrypt.compare(rt, user.hashedRt);
+    if (!rtMatch) throw new ForbiddenException('Acces Denied');
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await this.hashData(rt);
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
   }
 
   hashData(data: string) {
     return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string) {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        { secret: 'at-secret', expiresIn: 60 * 15 },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        { secret: 'rt-secret', expiresIn: 60 * 60 * 24 * 7 },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
   }
 }
